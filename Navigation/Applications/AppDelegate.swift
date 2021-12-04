62b43c6ab6d5e17dae7b5fbef021d270e9e6c620
Victor Dolgov <dolgov.vic95@gmail.com>
2021-12-04 15:59:05 +0700

    Deleted Main.storyboard

---
+++ Navigation/Applications/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Admin on 04.12.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        
+        return true
+    }
+
+}
+
